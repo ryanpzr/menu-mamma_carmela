46e5b033cfa40d1f394401641114cbb2c3e3ad62
ryanpzr <ryanpereiralimads@gmail.com>
2023-10-11 15:59:17 -0300

    Update_05_MediaQueryResolution

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,3 +3,19 @@ const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () =>
     header.classList.toggle("active"));
+    
+function redirect(idPrato) {
+    const list = document.querySelector(".list");
+    list.style.display = "none";
+
+    window.location.href = "#" + idPrato;
+}
+
+function display() {
+    
+    const list = document.querySelector(".list");
+    list.style.display = "block"
+
+}
+      
+  
